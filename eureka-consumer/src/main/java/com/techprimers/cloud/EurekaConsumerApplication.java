5f500c76f69ea66b9b706aa778215f2f316275e0
who <401262316@qq.com>
2019-10-02 11:46:05 +0800

    @EnableEurekaClient

--- eureka-consumer/src/main/java/com/techprimers/cloud/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/com/techprimers/cloud/EurekaConsumerApplication.java
@@ -3,10 +3,12 @@ package com.techprimers.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaConsumerApplication {
 
--- eureka-consumer/src/main/java/com/techprimers/cloud/HelloResource.java
+++ eureka-consumer/src/main/java/com/techprimers/cloud/HelloResource.java
@@ -1,5 +1,6 @@
 package com.techprimers.cloud;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,18 @@ public class HelloResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "fallback", groupKey = "hello",
+        commandKey = "hello", threadPoolKey = "helloThread")
+
     @GetMapping
     public String hello(){
         System.out.println("Before calling the service");
         String url = "http://eureka-provider/rest/hello/provider";
         return restTemplate.getForObject(url, String.class);
+    }
+
+    public String fallback(Throwable hystrixCommand){
+        return "Fall back hello world!";
     }
 
 }
